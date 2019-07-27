648c79c301e989e1b361bc85079d9ea0e3108d4f
Joseph Trettevik <josephtrettevik@gmail.com>
2019-07-27 18:45:16 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -30,8 +30,12 @@ def using_concat (array1, array2)
   array1.concat(array2)
 end
 
-def using_insert (array)
-  array.insert(3)
+def using_insert (array, element)
+  array.insert(3, element)
+end
+
+def using_uniq ()
+  
 end
 
 
